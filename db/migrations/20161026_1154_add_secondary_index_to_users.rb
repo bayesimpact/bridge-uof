3c3cf1d40b30b69d8a35f90ad9c71c76a256ca17
Alex Nisnevich <alex.nisnevich@gmail.com>
2016-11-01 15:25:01 -0700

    Fix secondary index migrations

--- db/migrations/20161026_1154_add_secondary_index_to_users.rb
+++ db/migrations/20161026_1154_add_secondary_index_to_users.rb
@@ -5,44 +5,51 @@ class AddSecondaryIndexToUsers < DynamoDB::Migration::Unit
 
     logger.info "Running migration #{self.class.name}:"
 
-    client.update_table(
-      table_name: "#{Dynamoid.config.namespace}_users",
-      attribute_definitions: [
-        {
-          attribute_name: "id",
-          attribute_type: "S"
-        },
-        {
-          attribute_name: "ori",
-          attribute_type: "S"
-        }
-      ],
-      provisioned_throughput: {
-        read_capacity_units: Dynamoid::Config.read_capacity,
-        write_capacity_units: Dynamoid::Config.write_capacity
-      },
-      global_secondary_index_updates: [
-        {
-          create: {
-            index_name: User.indexes['ori'].name,
-            key_schema: [
-              {
-                attribute_name: "ori",
-                key_type: "HASH"
+    table_name = "#{Dynamoid.config.namespace}_users"
+    index_name = User.indexes['ori'].name
+    existing_indexes = client.describe_table(table_name: table_name).table
+                             .global_secondary_indexes
+                             .flat_map {|idx| idx.index_name}
+
+    if existing_indexes.include? index_name
+      logger.info "Index #{index_name} already exists!"
+    else
+      logger.info "Creating index #{index_name} ..."
+      client.update_table(
+        table_name: table_name,
+        attribute_definitions: [
+          {
+            attribute_name: "id",
+            attribute_type: "S"
+          },
+          {
+            attribute_name: "ori",
+            attribute_type: "S"
+          }
+        ],
+        global_secondary_index_updates: [
+          {
+            create: {
+              index_name: index_name,
+              key_schema: [
+                {
+                  attribute_name: "ori",
+                  key_type: "HASH"
+                }
+              ],
+              projection: {
+                projection_type: "ALL",
+                non_key_attributes: []
+              },
+              provisioned_throughput: {
+                read_capacity_units: Dynamoid::Config.read_capacity,
+                write_capacity_units: Dynamoid::Config.write_capacity
               }
-            ],
-            projection: {
-              projection_type: "ALL",
-              non_key_attributes: []
-            },
-            provisioned_throughput: {
-              read_capacity_units: Dynamoid::Config.read_capacity,
-              write_capacity_units: Dynamoid::Config.write_capacity
             }
           }
-        }
-      ]
-    )
+        ]
+      )
+    end
 
     logger.info 'Done!'
     logger.info '================================'
