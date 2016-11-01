a90ce2c5028cb5e856db23cad986759ba328a066
Everett Wetchler <everett.wetchler@bayesimpact.org>
2016-10-31 18:32:50 -0700

    Fix tests

--- spec/requests/feedback_spec.rb
+++ spec/requests/feedback_spec.rb
@@ -10,8 +10,9 @@ describe '[Feedback and feedback email tests]', type: :request do
     expect(current_path).to eq(feedback_path)
   end
 
-  it 'Saves feedback and sends it in an email' do
+  it 'Saves feedback and sends it in an email', driver: :poltergeist do
     visit feedback_path
+    click 'Click here for a feedback form'
     fill_in 'Which part gave you difficulty', with: "Test source"
     fill_in 'Please explain', with: "Test content"
     expect { find('button[type=submit]').click }
