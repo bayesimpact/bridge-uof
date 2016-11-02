9824e86a4af593ba068f122c3de580bfbf4e1f26
Alex Nisnevich <alex.nisnevich@gmail.com>
2016-11-01 19:54:09 -0700

    Fix migrations (#31)
    * Fix rake tasks
    
    * typo
    
    * Fix secondary index migrations
    
    * Fix rubocop
    
    * Use our own dynamodb-migration fork; fix migration table name
    
    * oops

--- db/migrations/20161026_1148_add_secondary_index_to_incidents.rb
+++ db/migrations/20161026_1148_add_secondary_index_to_incidents.rb
@@ -5,44 +5,51 @@ class AddSecondaryIndexToIncidents < DynamoDB::Migration::Unit
 
     logger.info "Running migration #{self.class.name}:"
 
-    client.update_table(
-      table_name: "#{Dynamoid.config.namespace}_incidents",
-      attribute_definitions: [
-        {
-          attribute_name: "id",
-          attribute_type: "S"
-        },
-        {
-          attribute_name: "incident_id_str",
-          attribute_type: "S"
-        }
-      ],
-      provisioned_throughput: {
-        read_capacity_units: Dynamoid::Config.read_capacity,
-        write_capacity_units: Dynamoid::Config.write_capacity
-      },
-      global_secondary_index_updates: [
-        {
-          create: {
-            index_name: Incident.indexes['incident_id_str'].name,
-            key_schema: [
-              {
-                attribute_name: "incident_id_str",
-                key_type: "HASH"
+    table_name = "#{Dynamoid.config.namespace}_incidents"
+    index_name = Incident.indexes['incident_id_str'].name
+    existing_indexes = client.describe_table(table_name: table_name).table
+                             .global_secondary_indexes
+                             .flat_map(&:index_name)
+
+    if existing_indexes.include? index_name
+      logger.info "Index #{index_name} already exists!"
+    else
+      logger.info "Creating index #{index_name} ..."
+      client.update_table(
+        table_name: table_name,
+        attribute_definitions: [
+          {
+            attribute_name: "id",
+            attribute_type: "S"
+          },
+          {
+            attribute_name: "incident_id_str",
+            attribute_type: "S"
+          }
+        ],
+        global_secondary_index_updates: [
+          {
+            create: {
+              index_name: index_name,
+              key_schema: [
+                {
+                  attribute_name: "incident_id_str",
+                  key_type: "HASH"
+                }
+              ],
+              projection: {
+                projection_type: "INCLUDE",
+                non_key_attributes: ["id"]
+              },
+              provisioned_throughput: {
+                read_capacity_units: Dynamoid::Config.read_capacity,
+                write_capacity_units: Dynamoid::Config.write_capacity
               }
-            ],
-            projection: {
-              projection_type: "INCLUDE",
-              non_key_attributes: ["id"]
-            },
-            provisioned_throughput: {
-              read_capacity_units: Dynamoid::Config.read_capacity,
-              write_capacity_units: Dynamoid::Config.write_capacity
             }
           }
-        }
-      ]
-    )
+        ]
+      )
+    end
 
     logger.info 'Done!'
     logger.info '================================'
